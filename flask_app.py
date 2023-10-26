9cc8c45306edeb2150c6d0ceb1b8115f3b37c9e0
svniko <ya.pepulya@gmail.com>
2023-10-26 13:25:46 +0300

    lecture5

--- flask_app.py
+++ flask_app.py
@@ -1,15 +1,39 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b5de70f8ba9137f8bc7e5d1df81f1e70' 
 #'super puper secret key'
 
+app.config['SQLALCHEMY_DATABASE_URI'] =    	'sqlite:///user.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 	False
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, 	primary_key = True)
+    name = db.Column(db.String(200), nullable=False) 
+    email = db.Column(db.String(120), nullable=False, unique = True)
+    date_added = db.Column(db.DateTime, default = datetime.utcnow)
+   
+    def __repr__(self):
+        return f"<Name>: {self.name}"
+
 class FirstForm(FlaskForm):
     name = StringField("Enter your name, please", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+class UserForm(FlaskForm):
+    name=StringField("Name", validators=[DataRequired()])
+    email=StringField("Email", validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
 
 pets = [
     {
@@ -31,6 +55,71 @@ pets = [
         'color':'brown'
     },
 ]
+
+game = { }
+
+def init():
+    game['choise']=None
+    game['comp_win'] = 0
+    game['you_win'] =0
+    game['round'] = 0
+
+@app.route('/start/')
+def start():
+    init()
+    return render_template('rsp.jinja', start=True)
+
+@app.route('/game/')
+def rsp():
+    
+    if game['round'] < 5:
+        game['round'] +=1
+        print(game)
+        n = random.randint(0,2)
+        if game['choise'] == '0' and n == 0:
+            flash('Draw')
+        elif game['choise'] == '0' and n == 1:
+            flash('You win')
+            game['you_win'] +=1
+        elif game['choise'] == '0' and n == 2:
+            flash('Comp win')
+            game['comp_win'] += 1
+        else:
+            pass
+    
+    else:
+        if game['comp_win'] > game['you_win']:
+            flash('Total com win')
+        elif game['comp_win'] == game['you_win']:
+            flash('Total draw')
+        else:
+            flash('Total you win')
+    return render_template('rsp.jinja')
+
+@app.route('/select/<ch>')
+def select(ch):
+    game['choise'] = ch
+    return redirect(url_for('rsp'))
+
+@app.route('/user/add/', methods=['GET', 'POST'])
+def add_user():
+    form = UserForm()
+    our_users = []
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+            flash(f'User {form.name.data} added successfully', category='success')
+        else:
+            flash(f'User {form.name.data} cannot be added', category='danger')
+        
+        form.name.data = ''
+        form.email.data = ''
+        our_users = Users.query.order_by(Users.date_added)
+
+    return render_template('lecture5.jinja', form=form, our_users=our_users)
 
 
 @app.route("/")
